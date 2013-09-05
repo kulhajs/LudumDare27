ee22d4b986079d58335897a67217864147451509
Marek Mottl <marek@czechgeeks.cz>
2013-09-05 10:07:09 +0200

    prepared method for loading score from remote database

--- LD27/LD27/ScoreHandler.cs
+++ LD27/LD27/ScoreHandler.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LD27
 {
@@ -17,6 +19,13 @@ namespace LD27
         {
             Score score = new Score { DeviceId = deviceId, BestTime = time };
             Game1.MobileService.GetTable<Score>().InsertAsync(score);
+        }
+
+        public static bool LoadScore(string deviceId, float time)
+        {
+           var results = Game1.MobileService.GetTable<Score>().Where(score => score.DeviceId == deviceId).ToListAsync();
+
+            return true;
         }
     }
 }
